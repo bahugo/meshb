a4d58f4ebac50ef968c5359f908409edc5b912f9
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-07-22 11:34:17 +0200

    Added parser for node_name

--- src/mail_parser.rs
+++ src/mail_parser.rs
@@ -1,14 +1,25 @@
 use nom::{
   IResult,
-  bytes::complete::tag,
   branch::alt,
   multi::{many0, many1},
   number::complete::float,
   combinator::{opt, recognize, value},
-  sequence::{preceded, delimited, terminated, tuple},
-  character::complete::{char, one_of,multispace0, multispace1 },
+  sequence::{preceded, delimited, terminated, tuple, pair},
+  character::complete::{char, one_of,multispace0, multispace1, alpha1, digit1 },
 };
 
+
+    //  COOR_3D
+    //  N1        1.00000000000000E+00  4.00000000000000E+00  2.50000000000000E+00
+    //  N2        2.00000000000000E+00  4.00000000000000E+00  1.50000000000000E+00
+    //  N3        3.00000000000000E+00  4.00000000000000E+00  1.50000000000000E+00
+    //  N4        4.00000000000000E+00  4.00000000000000E+00  1.50000000000000E+00
+    // FINSF
+
+fn node_name(input: &str) -> IResult<&str, &str>{
+    let (input, name) = recognize(pair(alpha1, digit1))(input)?;
+    Ok((input, name))
+}
 
 fn node_3d_coords(input: &str) -> IResult<&str, [f32;3]> {
   let (input, (_, x, _, y, _, z, _)) = tuple((
@@ -32,10 +43,17 @@ mod tests{
     use insta::assert_debug_snapshot;
 
     #[test]
+    fn node_name_parser_should_work() {
+        assert_debug_snapshot!(node_name("23"));
+        assert_debug_snapshot!(node_name("N"));
+        assert_debug_snapshot!(node_name("N12"));
+        assert_debug_snapshot!(node_name("MD12"));
+    }
+    #[test]
     fn should_parse_node_3d_coords() {
         assert_debug_snapshot!(node_3d_coords("1 2 3"));
         assert_debug_snapshot!(node_3d_coords("   1  2 3 "));
         assert_debug_snapshot!(node_3d_coords("   1.0  2 3.01 "));
-
+        assert_debug_snapshot!(node_3d_coords("   1.0e1  2E+1 3.01E+00 "));
     }
 }
