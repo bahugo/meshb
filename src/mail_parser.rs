2170f2487883366440522bb12ccb19dff42a4b1e
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-07-23 17:08:07 +0200

    Added Poi1 parser and final parser

--- src/mail_parser.rs
+++ src/mail_parser.rs
@@ -1,9 +1,12 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, take},
-    character::complete::{alpha1, char, digit1, multispace0, multispace1, one_of},
+    character::complete::{
+        alpha1, anychar, char, digit1, line_ending, multispace0, multispace1, newline, one_of,
+        space0, space1,
+    },
     combinator::{opt, recognize, value},
-    multi::{many0, many1},
+    multi::{fold_many0, many0, many1},
     number::complete::float,
     sequence::{delimited, pair, preceded, terminated, tuple},
     IResult,
@@ -15,8 +18,37 @@ use nom::{
 //  N3        3.00000000000000E+00  4.00000000000000E+00  1.50000000000000E+00
 //  N4        4.00000000000000E+00  4.00000000000000E+00  1.50000000000000E+00
 // FINSF
+//
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum CellType {
+    POI1,
+    SEG2,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MailValue<'a> {
+    Null,
+    NodeElts(Vec<Node<'a>>),
+    Cells(Vec<CellProp<'a>>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct MailParseOutput<'a> {
+    pub nodes: Vec<Node<'a>>,
+    pub cells: Vec<CellProp<'a>>,
+}
+
+impl MailParseOutput<'_> {
+    fn new() -> Self {
+        MailParseOutput {
+            nodes: vec![],
+            cells: vec![],
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Node<'a> {
     pub name: &'a str,
     pub x: f32,
@@ -24,34 +56,64 @@ pub struct Node<'a> {
     pub z: f32,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct CellProp<'a> {
+    pub cell_type: CellType,
+    pub name: &'a str,
+    pub nodes: Vec<&'a str>,
+}
+
 fn node_name(input: &str) -> IResult<&str, &str> {
+    let (input, name) = recognize(pair(alpha1, digit1))(input)?;
+    Ok((input, name))
+}
+
+fn cell_name(input: &str) -> IResult<&str, &str> {
     let (input, name) = recognize(pair(alpha1, digit1))(input)?;
     Ok((input, name))
 }
 
 fn node_3d_coords(input: &str) -> IResult<&str, [f32; 3]> {
     let (input, (_, x, _, y, _, z, _)) = tuple((
-        multispace0,
+        space0,
         float,
         multispace1,
         float,
         multispace1,
         float,
-        multispace0,
+        space0,
     ))(input)?;
 
     Ok((input, [x, y, z]))
 }
 
 fn node_description(input: &str) -> IResult<&str, Node> {
-    let (input, (_, name, _, [x, y, z], _)) = tuple((
-        multispace0,
+    let (input, (_, name, _, [x, y, z], _, _)) = tuple((
+        space0,
         node_name,
-        multispace0,
+        space0,
         node_3d_coords,
-        multispace0,
+        space0,
+        line_ending,
     ))(input)?;
     Ok((input, Node { name, x, y, z }))
+}
+
+fn cell_description(cell_type: CellType, input: &str) -> IResult<&str, CellProp> {
+    let (input, (name, node_names, _, _)) = tuple((
+        cell_name,
+        many1(preceded(space1, node_name)),
+        space0,
+        line_ending,
+    ))(input)?;
+    Ok((
+        input,
+        CellProp {
+            cell_type,
+            name,
+            nodes: node_names,
+        },
+    ))
 }
 
 fn end_section_tag(input: &str) -> IResult<&str, &str> {
@@ -61,13 +123,50 @@ fn end_section_tag(input: &str) -> IResult<&str, &str> {
 fn start_3d_node_section(input: &str) -> IResult<&str, &str> {
     tag("COOR_3D")(input)
 }
-fn node_3d_section(input: &str) -> IResult<&str, Vec<Node>> {
-    let (input, (_, nodes, _)) = tuple((
-        start_3d_node_section,
-        many0(node_description),
+
+fn node_3d_section(input: &str) -> IResult<&str, MailValue> {
+    let (input, (_, _, nodes, _, _, _)) = tuple((
+        tuple((space0, start_3d_node_section, space0)),
+        many1(line_ending),
+        many0(preceded(multispace0, node_description)),
+        many0(line_ending),
         end_section_tag,
+        many0(line_ending),
     ))(input)?;
-    Ok((input, nodes))
+    Ok((input, MailValue::NodeElts(nodes)))
+}
+
+fn poi1_section(input: &str) -> IResult<&str, MailValue> {
+    let (input, (_, cells, _, _, _)) = tuple((
+        tuple((space0, tag("POI1"), space0)),
+        many0(preceded(multispace0, |input| {
+            cell_description(CellType::POI1, input)
+        })),
+        many0(line_ending),
+        end_section_tag,
+        many0(line_ending),
+    ))(input)?;
+    Ok((input, MailValue::Cells(cells)))
+}
+
+fn mail_final_parser(input: &str) -> IResult<&str, MailParseOutput> {
+    let mail_elt_parser = preceded(multispace0, alt((node_3d_section, poi1_section)));
+    let (input, output) = fold_many0(
+        mail_elt_parser,
+        MailParseOutput::new,
+        |mut acc: MailParseOutput, item| match item {
+            MailValue::NodeElts(nodes) => {
+                acc.nodes.extend(nodes);
+                acc
+            }
+            MailValue::Cells(cells) => {
+                acc.cells.extend(cells);
+                acc
+            }
+            _ => acc,
+        },
+    )(input)?;
+    Ok((input, output))
 }
 
 #[cfg(test)]
@@ -80,7 +179,7 @@ mod tests {
     fn node_description_parser_should_work() {
         assert_debug_snapshot!(node_description("23"));
         assert_debug_snapshot!(node_description("N12"));
-        assert_debug_snapshot!(node_description("N12 1.2  23.3 233"));
+        assert_debug_snapshot!(node_description("N12 1.2  23.3 233\n"));
         assert_debug_snapshot!(node_description("  N12 1.2  23.3 233\n"));
     }
 
@@ -112,8 +211,19 @@ mod tests {
     }
     #[test]
     fn node_3d_section_parser_should_work() {
-        assert_debug_snapshot!(node_3d_section("COOR_3D  \n\nN1 2  3.0 4\nFINSF"));
-        assert_debug_snapshot!(node_3d_section("COOR_3D  \nN1 2  3.0 4\nN2 3  4 4\nFINSF"));
-        assert_debug_snapshot!(node_3d_section("COOR_3D  \nN1 2  3.0 4\nBLABLA\nN2 3  4 4\nFINSF"));
+        assert_debug_snapshot!(node_3d_section("COOR_3D  \n\nN1 2  3.0 4\nFINSF\n"));
+        assert_debug_snapshot!(node_3d_section("COOR_3D\nN1 2  3.0 4\nN2 3  4 4\nFINSF\n"));
+    }
+    #[test]
+    fn poi1_section_parser_should_work() {
+        assert_debug_snapshot!(poi1_section("POI1  \n\nM1 N2   \nFINSF\n"));
+        assert_debug_snapshot!(poi1_section("POI1  \n\nM1 N2   \nM2 N3\nFINSF\n"));
+    }
+    #[test]
+    fn mail_final_parser_should_work() {
+        assert_debug_snapshot!(mail_final_parser(
+            "COOR_3D  \n\nN1 2  3.0 4\nFINSF\nCOOR_3D  \nN2 2  3.0 4\nN3 3  4 4\nFINSF"
+        ));
+        assert_debug_snapshot!(mail_final_parser("COOR_3D  \n\nN1 2  3.0 4\nFINSF\nPOI1\nM1 N1\nFINSF\n\nCOOR_3D  \nN2 2  3.0 4\nN3 3  4 4\nFINSF"));
     }
 }
